f7817a113120963a0e972ef6d7769ad970a9dda4
aiekick <aiekick@funparadigm.com>
2025-05-25 21:43:30 +0200

    -

---
+++ Tests/TestFile/TestEzJson.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+bool TestEzJson(const std::string& vTest);
---
+++ include/ezlibs/wip/ezJson.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+#include <cctype>
+#include <sstream>
+#include <stdexcept>
+
+namespace ez {
+class Json {
+public:
+    enum Type { Null, Object, Array, String, Number, Boolean };
+
+};
+
+} // namespace ez
